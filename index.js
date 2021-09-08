8fb338f3f702636e4dc3c6bc6adafa9ae24c407f
Эржигит Имамов <erjigit17@gmail.com>
2021-09-08 22:02:23 +0600

    find error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
 
 const PORT = process.env.PORT || 3000
 const URL = 'mongodb+srv://root:j!5hvm$5+-X9H9T@cluster0.mpxkg.mongodb.net/todos'
@@ -15,6 +16,7 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(todoRoutes)
 
 async function start() {
   try {
