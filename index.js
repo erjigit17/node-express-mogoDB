6cf6dbd4c01f462c6fba1f2570271ae3945c3cb8
Эржигит Имамов <erjigit17@gmail.com>
2021-09-09 08:19:54 +0600

    comlete

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const exphbs = require('express-handlebars')
 const todoRoutes = require('./routes/todos')
 
 const PORT = process.env.PORT || 3000
-const URL = 'mongodb+srv://root:j!5hvm$5+-X9H9T@cluster0.mpxkg.mongodb.net/todos'
 
 const app = express()
 const hbs = exphbs.create({
@@ -16,22 +16,26 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(todoRoutes)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(PORT, () => {
-  console.log('Server has been started...' + PORT)
-})
+app.use(todoRoutes)
 
 async function start() {
   try {
-    await mongoose.connect(URL, {
-      userNewUrlParser: true,
-      userFindAndModify: false,
-    })
+    await mongoose.connect(
+      'mongodb+srv://root:j!5hvm$5+-X9H9T@cluster0.mpxkg.mongodb.net/myFirstDatabase',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    )
     app.listen(PORT, () => {
-      console.log('Server has been started...' + PORT)
+      console.log('Server has been started...')
     })
   } catch (e) {
     console.log(e)
   }
 }
+
+start()
